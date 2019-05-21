9d604d016844bde26660bed6f678fa62d5f0f3ad
lichunyin <licy2013@aliyun.com>
2019-05-21 16:56:49 +0800

    loading assets

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
+import './index.css';
 
 function component(){
     var element         = document.createElement('div');
     element.innerHTML   = _.join(['hello', 'word'], ' ');
+    element.classList.add('color-red');
+    element.classList.add('background');
     return element;
 }
 
